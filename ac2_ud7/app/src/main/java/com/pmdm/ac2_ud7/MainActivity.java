164b91e16d5c95e959f2ed213d3ae1c4aec07980
Selu <joseluispermag@gmail.com>
2024-11-28 14:37:35 +0100

    UPD: notificacion

--- ac2_ud7/app/src/main/java/com/pmdm/ac2_ud7/MainActivity.java
+++ ac2_ud7/app/src/main/java/com/pmdm/ac2_ud7/MainActivity.java
@@ -1,15 +1,25 @@
 package com.pmdm.ac2_ud7;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,11 +27,58 @@ import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btNotification;
+    private PendingIntent pendingIntent;
+    private static final String CHANNEL_ID = "NOTIFICACION";
+    private static final int NOTIFICATION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btNotification = findViewById(R.id.botonNtf);
+        btNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotificationChannel();
+                createNotification();
+            }
+        });
+
+    }
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Notificacion";
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+    private void createNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+            builder.setSmallIcon(R.drawable.ic_noti);
+            builder.setContentTitle("AIRPLANE");
+            builder.setContentText("Son las 9 y 11");
+            builder.setColor(Color.BLUE);
+            builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+            builder.setLights(Color.MAGENTA,100,100);
+            builder.setVibrate(new long[]{1000,1000,1000,1000,1000});
+            builder.setDefaults(Notification.DEFAULT_SOUND);
+
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 101);
+            }
+        }
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
+}
+
+        /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
             if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 101);
             }
@@ -58,7 +115,4 @@ public class MainActivity extends AppCompatActivity {
                 notificationManager.notify(1, builder.build());
                 Log.d("DEBUG", "Notificación enviada");
             });
-        }
-    }
-
-}
+        }*/
